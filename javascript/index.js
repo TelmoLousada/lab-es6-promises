319f73a2d7d29624e62404d9d14a338c664a5237
Telmo Lousada <telmo.lousada@gmail.com>
2025-02-11 05:52:22 +0000

    Add iteration 1 and 3

--- javascript/index.js
+++ javascript/index.js
@@ -4,9 +4,8 @@
 // 🚨🚨🚨 Comment out the below code before you start working on the code
 
 // Out of sync
-  getInstruction("mashedPotatoes", 0, (step1) => {
+  /*getInstruction("mashedPotatoes", 0, (step1) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-  }, (error) => console.log(error));
   
   getInstruction("mashedPotatoes", 1, (step2) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
@@ -23,18 +22,73 @@
   getInstruction("mashedPotatoes", 4, (step5) => {
     document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
     document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-  }, (error) => console.log(error));
+  }, (error) => console.log(error));*/
 
-
+  
 
 // Iteration 1 - using callbacks
 // ...
+
+  getInstruction("mashedPotatoes", 0, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+
+    getInstruction("mashedPotatoes", 1, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+
+      getInstruction("mashedPotatoes", 2, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+
+        getInstruction("mashedPotatoes", 3, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+
+          getInstruction("mashedPotatoes", 4, (step5) => {
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
+
+            document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
+
+            document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+          }, (error) => console.log(error));
+
+        }, (error) => console.log(error));
+
+      }, (error) => console.log(error));
+
+    }, (error) => console.log(error));
+
+  }, (error) => console.log(error));
 
 // Iteration 2 - using promises
 // ...
 
 // Iteration 3 using async/await
 // ...
+async function makeBroccoli() {
+  try{
+    const broccoli0 = await obtainInstruction("broccoli", 0);
+    const broccoli1 = await obtainInstruction("broccoli", 1);
+    const broccoli2 = await obtainInstruction("broccoli", 2);
+    const broccoli3 = await obtainInstruction("broccoli", 3);
+    const broccoli4 = await obtainInstruction("broccoli", 4);
+    const broccoli5 = await obtainInstruction("broccoli", 5);
+
+    document.querySelector("#broccoli").innerHTML += `<li>${broccoli0}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>${broccoli1}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>${broccoli2}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>${broccoli3}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>${broccoli4}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>${broccoli5}</li>`;
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+  }
+
+  catch(error) {
+    console.log(error);
+  }
+}
+
+makeBroccoli();
+
+
 
 // Bonus 2 - Promise all
 // ...
